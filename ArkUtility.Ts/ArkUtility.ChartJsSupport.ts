ea6c2b10c1d53260ace23b9ac606549ae3f0332a
Sean J <webdevx@gmail.com>
2018-01-23 16:06:38 -0500

    Update ArkUtility.ChartJsSupport.ts

--- ArkUtility.Ts/ArkUtility.ChartJsSupport.ts
+++ ArkUtility.Ts/ArkUtility.ChartJsSupport.ts
@@ -1,20 +1,21 @@
-﻿/* Copyright 2018 Sean Jordan
-(Licence Notice (LGPLv3))
+/* Copyright 2018 Sean Jordan
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-This file is part of ArkUtility.Ts a TypeScript Project.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-ArkUtility.Ts is free software: you can redistribute it and / or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-ArkUtility.Ts is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with ArkUtility.Ts. If not, see < http://www.gnu.org/licenses/ >.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
 /* Typedefs have been modified to permit TypeScript 1.8 compiling due to local requirements not yet permitting TypeScript v2+. Upon upgrade re-download typedefs for chart.js and jquery */
